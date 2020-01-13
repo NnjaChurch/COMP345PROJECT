e2989de27a5d22584523c98635c123af0de3fe76
Kevin McAllister <kevinryan@videotron.ca>
2020-01-13 18:49:49 -0500

    Added Template files

---
+++ headers/Header.h
@@ -0,0 +1,15 @@
+/*
+	Header Template
+	COMP 345 Project
+	@author: Kevin McAllister (40031326)
+*/
+// Header Guard (Visual Studio also has #pragma once, but these are the more standard header guards that make the code useable outside of the visual studio compiler)
+#ifndef HEADER_H
+#define HEADER_H
+// Includes
+
+// Namespaces
+
+// 3 Main types of headers: Class, Namespace, Function (headers can have multiple in each, but can become cluttered)
+
+#endif // !HEADER_H
