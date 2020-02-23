41534089bc1d58c1a4ecdc6437bfd2cbd7b599f8
annesreus <46769308+annesreus@users.noreply.github.com>
2020-02-23 12:24:01 -0500

    -Added some methods that need to be implemented

--- headers/Player.h
+++ headers/Player.h
@@ -1,5 +1,29 @@
 #pragma once
+// Includes
+#include <vector>
+#include <iostream>
+
+// Namespaces
+using namespace std;
 class Player
-{
+{   
+    public:
+    void PlaceHarvestTile(int board_space);
+    void DrawBuilding(int PlayerNumber);
+    void DrawHarvestTile();
+    void ResourceTracker();
+    void BuildVillage();
+    void CalculateResources();
+
+    // Default Constructor
+	Player(int player_number);
+    // Destructor
+	virtual ~Player();
+
+    private:
+    string* villageName;
+    vector<int>* resourceMarkers[4];
+    vector<int>* harvest_tiles[];
+    vector<int>* building_tokens[];
 };
 
