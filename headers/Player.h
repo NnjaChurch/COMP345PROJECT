98ae3c9225274d4397c8631af78bb292a1d506dd
annesreus <46769308+annesreus@users.noreply.github.com>
2020-02-27 22:03:41 -0500

    - Cleaned up a bit still trying

--- headers/Player.h
+++ headers/Player.h
@@ -10,17 +10,19 @@ class Player
    
 
 public:
-    void PlaceHarvestTile(int board_space);
-    void DrawBuilding(int PlayerNumber);
+    
+    // Default Constructor
+    Player(int player_number);
+    // Destructor
+    virtual ~Player();
+
+    string PlaceHarvestTile();
+    void DrawBuilding();
     void DrawHarvestTile();
     void ResourceTracker();
     void BuildVillage();
     void CalculateResources();
 
-    // Default Constructor
-    Player(int player_number);
-    // Destructor
-    virtual ~Player();
 
 private:
     string *villageName;
@@ -28,37 +30,41 @@ private:
     vector<int> *harvest_tiles();
     vector<int> *building_tokens();
 };
-class TempVGMap
-    {
+
+class TempVGMap {
+public:
+	// Node Class
+	class VGNode {
+		// Will contain Tile data
+	public:
+		VGNode(int node_number);
+		virtual ~VGNode();
+	private:
+		int NodeNumber;
+	};
+
+	// Default Constructor
+	TempVGMap(int player_number);
+	// Constructor for Map Loader
+	TempVGMap(int player_number, vector<VGNode*>* nodes, vector<vector<int>>* edges);
+	// Destructor
+	virtual ~TempVGMap();
+
+	// Player Functions
+	void AddTile(int board_space);		// Missing Tile from Resource
+
+	// Draw Functions
+	void Draw();	// Outputs Board to the Console
+
+private:
+	int* PlayerNumber;
+	int* NumNodes;
+	vector<VGNode*>* Nodes;
+	vector<vector<int>>* Edges;
+};
+class TempResources{
     public:
-        // Node Class
-        class TempTempsVGNode
-        {
-            // Will contain Tile data
-        public:
-            TempsVGNode(int node_number);
-            virtual ~TempsVGNode();
-
-        private:
-            int NodeNumber;
-        };
-
-        // Default Constructor
-        TempsVGMap(int player_number);
-        // Constructor for Map Loader
-        TempsVGMap(int player_number, vector<TempsVGNode *> *nodes, vector<vector<int>> *edges);
-        // Destructor
-        virtual ~TempsVGMap();
-
-        // Player Functions
-        void AddTile(int board_space); // Missing Tile from Resource
-
-        // Draw Functions
-        void Draw(); // Outputs Board to the Console
-
     private:
-        int *PlayerNumber;
-        int *NumNodes;
-        vector<TempsVGNode*> *Nodes;
-        vector<vector<int>> *Edges;
-    };
+    vector<int>* villageBuildings;
+
+};
