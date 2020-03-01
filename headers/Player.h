20a280829a3b6edd10bb653324baa6f95c52fa79
annesreus <46769308+annesreus@users.noreply.github.com>
2020-03-01 16:48:47 -0500

    Add files via upload

--- headers/Player.h
+++ headers/Player.h
@@ -1,34 +1,36 @@
 #pragma once
 // Includes
-#include <vector>
 #include <iostream>
-
+#include<vector> 
+#include<ctime>
+#include<stack>
 // Namespaces
 using namespace std;
-class Player {
+class Player
+{
 public:
+    
 	// Default Constructor
-	Player(int player_number);
-	// Destructor
-	virtual ~Player();
-
-	string PlaceHarvestTile();
-	void DrawBuilding();
-	void DrawHarvestTile();
-	void ResourceTracker();
-	void BuildVillage();
-	void CalculateResources();
+    Player(int player_number);
+    // Destructor
+    virtual ~Player();
+    void PlaceHarvestTile();
+    void DrawBuilding();
+    void DrawHarvestTile();
+    void ResourceTracker();
+    void BuildVillage();
+    void CalculateResources();
 
 
 private:
-	string *villageName;
-	vector<int> *resourceMarkers;
-	vector<int> *harvest_tiles;
-	vector<int> *building_tokens;
+    string *villageName;
+    vector<int> *resourceMarkers();
+    vector<int> *harvest_tiles();
+    vector<int> *building_tokens();
 };
 
-class TempVGMap {
-public:
+class VGMap {
+	public:
 	// Node Class
 	class VGNode {
 		// Will contain Tile data
@@ -40,27 +42,74 @@ public:
 	};
 
 	// Default Constructor
-	TempVGMap(int player_number);
+	VGMap(int player_number);
 	// Constructor for Map Loader
-	TempVGMap(int player_number, vector<VGNode*>* nodes, vector<vector<int>>* edges);
+	VGMap(int player_number, vector<VGNode*>* nodes, vector<vector<int>>* edges);
 	// Destructor
-	virtual ~TempVGMap();
+	virtual ~VGMap();
 
 	// Player Functions
 	void AddTile(int board_space);		// Missing Tile from Resource
-
+	
 	// Draw Functions
 	void Draw();	// Outputs Board to the Console
 
-private:
+	private:
 	int* PlayerNumber;
 	int* NumNodes;
 	vector<VGNode*>* Nodes;
 	vector<vector<int>>* Edges;
 };
-class TempResources{
-	public:
-	private:
-	vector<int>* villageBuildings;
+struct Building;
 
+
+class TileDeck {
+	public:
+		stack<vector<char>> deck;
+		Building draw(stack<Building>& deck);
+		void BuildingDeck();
+		char getTopLeftOfTile(vector<char>);
+		char getTopRightOfTile(vector<char>);
+		char getBottomLeftOfTile(vector<char>);
+		char getBottomRightOfTile(vector<char>);
+		TileDeck(){
+     		vector < vector<char>> temp = allCard();
+        	fullShuffle(temp);
+        	deck = createDeck(temp);
+		};
+		void rotate(vector<char>& arr);
+		vector < vector<char>> allCard();
+		void shuffle(vector <vector<char>>& vec, int a, int b);
+		void fullShuffle(vector <vector<char>>& vec);
+		stack<vector<char>> createDeck(vector<vector<char>> cards);
+		vector<char> draw(stack<vector<char>>& deck);
+        
+	private:
+		
+
+
+};
+
+
+class BuildingDeck {
+
+	public:
+		BuildingDeck();
+		Building draw(stack<struct Building>& deck);
+
+	private:
+		stack<struct Building> deckOfBuilding;
+		void shuffle(vector<struct Building>& vec, int a, int b);
+		void fullShuffle(vector<struct Building>& vec);
+		stack<struct Building> createDeck(vector<struct Building> cards);
+
+
+
+
+};
+
+struct Building
+{
+	int value;
+    char color;
 };
