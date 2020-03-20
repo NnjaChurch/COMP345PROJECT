b63c53b13e728902fabb94109a4073729f8d33c7
annesreus <46769308+annesreus@users.noreply.github.com>
2020-03-20 11:12:38 -0400

    Fixed the headers and added decks

--- src/TurnSequence.cpp
+++ src/TurnSequence.cpp
@@ -1,8 +1,8 @@
-#include <VGMap.h>
-#include <Resources.h>
-#include <Player.h>
-#include <GBMap.h>
-#include <GBMapLoader.h>
+#include "../headers/VGMap.h"
+#include "../headers/Resources.h"
+#include "../headers/Player.h"
+#include "../headers/GBMap.h"
+#include "../headers/GBMapLoader.h"
 #include <iostream>
 #include <vector>
 
@@ -14,15 +14,36 @@ int main(){
     GBMapLoader* GBMapNumP = new GBMapLoader();
     GBMapNumP->LoadMap(number_of_players); //We have now created a Map depending on the number of players.
     cout<<"Map loaded successfully!";
+    //Creating Deck
+    Deck* DeckTiles= new Deck(HARVEST);
+    Deck* DeckTiles= new Deck(BUILDING);
+    cout<<"Deck created successfully";
+    
     
     //creating the players
     vector<Player*> Players;
+    vector<Resource*> ResourcesForPlayer;
     for (int i = 1; i <= number_of_players; i++)
     {
         Players [i]= new Player (i); 
+        ResourcesForPlayer [i]= new Resource();
+
     }
     //Players have been initialized
-    
+    //main loop PART 2
+    int counter;
+    while (counter!=0)
+    {   
+        for (int i = 1; i <= number_of_players; i++)
+        {
+            cout<<"It's the turn for player "<< i << endl ;
+            cout<<"Here are the tiles that you pocess: "<<endl;
+
+        }
+        
+    }
+    //end of main loop
+    //
 
 
     
