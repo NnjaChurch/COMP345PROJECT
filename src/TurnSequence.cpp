77fadc8d4b87863c523ebefcd200467c5f661889
annesreus <46769308+annesreus@users.noreply.github.com>
2020-03-22 12:23:43 -0400

    Progressed in the turnsequence.
    Removed the vectors and started using only 1 villager, we can add the others when that one player works.

--- src/TurnSequence.cpp
+++ src/TurnSequence.cpp
@@ -38,16 +38,30 @@ int main(){
     while (counter!=0)
     {   
         for (int i = 1; i <= number_of_players; i++)
-        {   int tile_to_place,where_to_place;
+        {   int tile_to_place,where_to_place,building_to_place,where_to_placeBuilding;
 
             cout<<"It's the turn for player "<< i << endl ;
             cout<<"Here are the tiles that you pocess: "<<endl;
-            cout<<"Which tile do you want to place?"<<endl;
+            cout<<"Which tile do you want to place?"<<endl; //TODO: Need to include that jokerTile special tile option
             cin>>tile_to_place;
             cout<<"Where do you want to place it?"<<endl;
             cin>>where_to_place;
             villager->PlaceHarvestTile(tile_to_place,where_to_place,GBMapper);
+            cout<<"The Tile has been placed"<<endl;
+            cout<<"Calculating resources..."<<endl;
+            villager->CalculateResources(tile_to_place,GBMapper);
+            villager->ResourceTracker(tile_to_place,GBMapper);
+            cout<<"Village Building phase";
+            cout<<"Here are the buildings that you pocess: "<<endl;
+            cout<<"Which building do you want to place?"<<endl;
+            cin>>building_to_place;
+            cout<<"Where do you want to place it?"<<endl;
+            cin>>where_to_placeBuilding;
+            villager->BuildVillage( where_to_placeBuilding, building_to_place);
+            cout<<"Building has been placed"<<endl;
 
+            //TODO: each other player in clockwise order will have the option of also using any 
+            //remaining resources to construct buildings in their own village.
             
 
         }
